e9b3d422ff51f96d9cd0fd7c79eaad1af132f256
Yoan-Alexander Grigorov <joan.grigorov@gmail.com>
2014-03-10 21:57:38 +0200

    Removed getStep() since it's not really needed

--- src/PhlyMongo/RangedPaginatorAdapterInterface.php
+++ src/PhlyMongo/RangedPaginatorAdapterInterface.php
@@ -13,14 +13,6 @@ interface RangedPaginatorAdapterInterface
     public function getCurrentId();
 
     /**
-     * Step determines the direction (prev or next) of the moving
-     * and the absolute value of step is the number of pages to skip
-     *
-     * @return integer
-     */
-    public function getStep();
-
-    /**
      * Step limit
      *
      * Since the step is implemented using skip() we need to have some limitation on this
